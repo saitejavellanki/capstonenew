1cf89e43ba94dcd605b0ea2d00b3e99b988c2899
saitejavellanki <95115567+saitejavellanki@users.noreply.github.com>
2025-01-17 15:44:05 +0530

    bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// Updated App.js
 import React, { useState, useEffect } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
@@ -20,7 +19,7 @@ import Footer from './Components/footer/Footer';
 import OrderPickupConfirmation from './Components/order/OrderConfirmation';
 import UserProfile from './pages/user/UserProfile';
 import VendorDashboard from './pages/vendors/workflow';
-import ScrollToTop from './pages/utils/ScrollToTop'; // Add this import
+import ScrollToTop from './pages/utils/ScrollToTop';
 import AboutUs from './pages/utils/AboutUs';
 import HowItWorks from './pages/utils/HowItWorks';
 import ContactUs from './pages/utils/ContactUs';
@@ -28,13 +27,19 @@ import HelpCenter from './pages/utils/HelpCenter';
 import PrivacyPolicy from './pages/utils/PrivacyPolicy';
 import TermsAndConditions from './pages/utils/TermsAndConditions';
 import LoadingScreen from './pages/utils/loading';
-// import FostPolicyPage from './pages/utils/Refund';
 import CancellationPolicy from './pages/utils/Refund';
 import PaymentSuccess from './pages/utils/PaymentSuccess';
 import Reset from './pages/Auth/Reset';
 import VendorCounterOrder from './pages/vendors/VendorCounterOrder';
 import AddCouponForm from './pages/utils/AddCoupon';
-
+import ProcessingOrders from './pages/vendors/ProcessingVendor';
+import PendingOrders from './pages/vendors/PendingVendor';
+import OrderScanningPage from './pages/vendors/CompletedVendor';
+import VendorLayout from './layouts/VendorLayout';
+import AnalyticsDashboard from './pages/vendors/AnalyticsVendor';
+import AdvancedAnalytics from './pages/vendors/AdvancedAnalytic';
+import OrderHistory from './pages/vendors/HistoryVendors';
+import KPIDashboard from './pages/vendors/KPIVendors';
 
 const ProtectedRoute = ({ children, requiredRole }) => {
   const [user, setUser] = useState(null);
@@ -70,114 +75,69 @@ const ProtectedRoute = ({ children, requiredRole }) => {
   return children;
 };
 
-const NotFound = () => {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      window.location.href = '/';
-    }, 100);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return null;
-};
-
 function App() {
   return (
     <ChakraProvider>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <ScrollToTop /> 
-        
+        <ScrollToTop />
         <LoadingScreen>
-        <Navbar />
-        <Routes>
-        
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/reset-password" element={<Reset />} />
-          <Route path="/order-waiting/:orderid" element={<OrderWaitingPage />} />
-          <Route path="/order-confirmation" element={<OrderPickupConfirmation />} />
-          <Route path="/Profile" element={<UserProfile />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/HowItWorks" element={<HowItWorks />} />
-          <Route path="/Contactus" element={<ContactUs />} />
-          <Route path="/helpcenter" element={<HelpCenter />} />
-          <Route path="/privacypolicy" element={<PrivacyPolicy/>} />
-          <Route path="/termsandconditions" element={<TermsAndConditions />} />
-          <Route path="/refund" element={<CancellationPolicy />} />
-          <Route path="/payment-success/:shopId" element={<PaymentSuccess />} />
-          <Route path="/counter" element={<VendorCounterOrder />} />
-          <Route path="/addcoupon" element={<AddCouponForm />} />
-          <Route 
-            path="/vendor/dashboard" 
-            element={
-              <ProtectedRoute requiredRole="vendor">
-                <VendorDashboard />
-              </ProtectedRoute>
-            } 
-          />
+          <Navbar />
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/reset-password" element={<Reset />} />
+            <Route path="/order-waiting/:orderid" element={<OrderWaitingPage />} />
+            <Route path="/order-confirmation" element={<OrderPickupConfirmation />} />
+            <Route path="/Profile" element={<UserProfile />} />
+            <Route path="/AboutUs" element={<AboutUs />} />
+            <Route path="/HowItWorks" element={<HowItWorks />} />
+            <Route path="/Contactus" element={<ContactUs />} />
+            <Route path="/helpcenter" element={<HelpCenter />} />
+            <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+            <Route path="/termsandconditions" element={<TermsAndConditions />} />
+            <Route path="/refund" element={<CancellationPolicy />} />
+            <Route path="/payment-success/:shopId" element={<PaymentSuccess />} />
+            <Route path="/counter" element={<VendorCounterOrder />} />
+            <Route path="/addcoupon" element={<AddCouponForm />} />
 
-          <Route
-            path="/main"
-            element={
-              <ProtectedRoute>
-                <Main />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/shop/:shopId"
-            element={
-              <ProtectedRoute>
-                <Shop />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/cart"
-            element={
-              <ProtectedRoute>
-                <Cart />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/orders"
-            element={
-              <ProtectedRoute>
-                <UserOrders />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/shops"
-            element={
+            {/* Protected User Routes */}
+            <Route path="/main" element={<ProtectedRoute><Main /></ProtectedRoute>} />
+            <Route path="/shop/:shopId" element={<ProtectedRoute><Shop /></ProtectedRoute>} />
+            <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+            <Route path="/orders" element={<ProtectedRoute><UserOrders /></ProtectedRoute>} />
+
+            {/* Admin Routes */}
+            <Route path="/admin/shops" element={
               <ProtectedRoute requiredRole="admin">
                 <AdminShops />
               </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/vendor/items"
-            element={
+            } />
+
+            {/* Vendor Routes */}
+            <Route path="/vendor" element={
               <ProtectedRoute requiredRole="vendor">
-                <VendorItems />
+                <VendorLayout />
               </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/vendor/orders"
-            element={
-              <ProtectedRoute requiredRole="vendor">
-                <VendorOrders />
-              </ProtectedRoute>
-            }
-          />
-          
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+            }>
+              <Route path="dashboard" element={<VendorDashboard />} />
+              <Route path="items" element={<VendorItems />} />
+              <Route path="orders" element={<VendorOrders />} />
+              <Route path="ProcessingVendors" element={<ProcessingOrders />} />
+              <Route path="PendingVendors" element={<PendingOrders />} />
+              <Route path="CompletedVendor" element={<OrderScanningPage />} />
+              <Route path="AnalyticVendor" element={<AnalyticsDashboard/>} />
+              <Route path="AdvancedAnalytics" element={<AdvancedAnalytics/>} />
+              <Route path="HistoryVendor" element={<OrderHistory/>} />
+              <Route path="KpiVendor" element={<KPIDashboard/>} />
+            </Route>
+
+            {/* Catch-all redirect */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
         </LoadingScreen>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </ChakraProvider>
   );
