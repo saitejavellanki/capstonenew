3656165a30abd9505da1efdbc9b5853ec5136121
saitejavellanki <95115567+saitejavellanki@users.noreply.github.com>
2025-02-03 08:04:58 +0530

    updated bugs in login

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import {
   Box,
   Flex,
-  Stack,
   VStack,
   Heading,
   Input,
@@ -18,7 +17,7 @@ import {
   Icon,
   InputGroup,
   InputLeftElement,
-  HStack,
+  Container,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
@@ -114,44 +113,43 @@ const Login = () => {
   };
 
   return (
-    <Flex minH="100vh" direction={{ base: "column", md: "row" }}>
+    <Flex 
+      minH={{ base: "100vh", md: "100vh" }} 
+      direction={{ base: "column", md: "row" }}
+    >
       {/* Left Side - Hero Section */}
       <Box
-        display={{ base: "none", md: "flex" }}
-        flex="1"
+        display={{ base: "flex", md: "flex" }}
+        flex={{ base: "none", md: "1" }}
         bg="orange.500"
+        position="relative"
+        overflow="hidden"
+        minH={{ base: "120px", md: "auto" }}
+        py={{ base: 8, md: 0 }}
       >
         <VStack
           w="full"
           h="full"
           justify="center"
-          p={10}
-          spacing={6}
+          p={{ base: 4, md: 10 }}
+          spacing={{ base: 2, md: 6 }}
           color="white"
         >
-          <Heading size="2xl" fontWeight="bold" textAlign="center">
-            Welcome to Our Marketplace
+          <Heading 
+            size={{ base: "lg", md: "2xl" }} 
+            fontWeight="bold" 
+            textAlign="center"
+          >
+            Welcome Back
           </Heading>
-          <Text fontSize="xl" textAlign="center" maxW="500px">
-            Join thousands of successful entrepreneurs and customers in our thriving marketplace
+          <Text 
+            fontSize={{ base: "sm", md: "xl" }} 
+            textAlign="center" 
+            maxW="400px"
+            display={{ base: "none", md: "block" }}
+          >
+            Your trusted marketplace for quality products and services
           </Text>
-          
-          <Stack spacing={6} mt={4}>
-            <HStack spacing={4} justify="center">
-              <Box p={4} bg="whiteAlpha.200" borderRadius="lg">
-                <Text fontWeight="bold">100K+</Text>
-                <Text fontSize="sm">Active Users</Text>
-              </Box>
-              <Box p={4} bg="whiteAlpha.200" borderRadius="lg">
-                <Text fontWeight="bold">50K+</Text>
-                <Text fontSize="sm">Products</Text>
-              </Box>
-              <Box p={4} bg="whiteAlpha.200" borderRadius="lg">
-                <Text fontWeight="bold">95%</Text>
-                <Text fontSize="sm">Satisfaction</Text>
-              </Box>
-            </HStack>
-          </Stack>
         </VStack>
       </Box>
 
@@ -161,129 +159,157 @@ const Login = () => {
         bg="white"
         justify="center"
         align="center"
-        p={{ base: 4, md: 6, lg: 8 }}
+        p={{ base: 4, sm: 6, md: 8, lg: 12 }}
       >
-        <VStack
-          w="full"
-          maxW="440px"
-          spacing={4}
-        >
-          <VStack spacing={1} align="flex-start" w="full">
-            <Heading fontSize="3xl" color="gray.800">
-              Sign In
-            </Heading>
-            <Text color="gray.600">
-              Don't have an account?{" "}
+        <Container maxW={{ base: "100%", sm: "400px" }} px={{ base: 4, sm: 0 }}>
+          <VStack spacing={{ base: 4, md: 6 }} w="full">
+            <VStack spacing={2} align="flex-start" w="full">
+              <Heading 
+                fontSize={{ base: "xl", md: "2xl" }} 
+                color="gray.800"
+              >
+                Sign In
+              </Heading>
+              <Text 
+                color="gray.600" 
+                fontSize={{ base: "sm", md: "md" }}
+              >
+                Don't have an account?{" "}
+                <Link
+                  color="orange.500"
+                  fontWeight="semibold"
+                  _hover={{ textDecoration: "none", color: "orange.600" }}
+                  onClick={() => navigate("/register")}
+                >
+                  Join Now
+                </Link>
+              </Text>
+            </VStack>
+
+            {loginError && (
+              <Alert 
+                status="error" 
+                borderRadius="md"
+                fontSize={{ base: "sm", md: "md" }}
+              >
+                <AlertIcon />
+                <AlertDescription>{loginError}</AlertDescription>
+              </Alert>
+            )}
+
+            <VStack spacing={4} w="full">
+              <FormControl>
+                <InputGroup size={{ base: "md", md: "lg" }}>
+                  <InputLeftElement pointerEvents="none">
+                    <Icon as={MdEmail} color="gray.400" />
+                  </InputLeftElement>
+                  <Input
+                    type="email"
+                    placeholder="Email Address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    borderRadius="md"
+                    _focus={{
+                      borderColor: "orange.500",
+                      boxShadow: "0 0 0 1px orange.500",
+                    }}
+                  />
+                </InputGroup>
+              </FormControl>
+
+              <FormControl>
+                <InputGroup size={{ base: "md", md: "lg" }}>
+                  <InputLeftElement pointerEvents="none">
+                    <Icon as={MdLock} color="gray.400" />
+                  </InputLeftElement>
+                  <Input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    borderRadius="md"
+                    _focus={{
+                      borderColor: "orange.500",
+                      boxShadow: "0 0 0 1px orange.500",
+                    }}
+                  />
+                </InputGroup>
+              </FormControl>
+
+              <Flex w="full" justify="flex-end">
+                <Link
+                  color="orange.500"
+                  fontSize={{ base: "sm", md: "sm" }}
+                  _hover={{ textDecoration: "none", color: "orange.600" }}
+                  onClick={() => navigate("/reset-password")}
+                >
+                  Forgot Password?
+                </Link>
+              </Flex>
+
+              <Button
+                w="full"
+                size={{ base: "md", md: "lg" }}
+                colorScheme="orange"
+                isLoading={isLoading}
+                onClick={() => handleSignIn("email")}
+                borderRadius="md"
+              >
+                Sign In
+              </Button>
+
+              <Flex align="center" w="full" my={{ base: 2, md: 2 }}>
+                <Divider borderColor="gray.200" />
+                <Text 
+                  px={4} 
+                  color="gray.500" 
+                  fontSize={{ base: "sm", md: "sm" }}
+                >
+                  or
+                </Text>
+                <Divider borderColor="gray.200" />
+              </Flex>
+
+              <Button
+                w="full"
+                size={{ base: "md", md: "lg" }}
+                variant="outline"
+                leftIcon={<Icon as={FcGoogle} boxSize={5} />}
+                onClick={() => handleSignIn("google")}
+                isLoading={isLoading}
+                borderRadius="md"
+                borderColor="gray.200"
+                _hover={{ bg: "gray.50" }}
+              >
+                Continue with Google
+              </Button>
+            </VStack>
+
+            <Text 
+              fontSize={{ base: "xs", md: "sm" }} 
+              color="gray.500" 
+              textAlign="center"
+              mt={{ base: 2, md: 4 }}
+            >
+              By signing in, you agree to our{" "}
               <Link
                 color="orange.500"
-                fontWeight="semibold"
-                _hover={{ color: "orange.600" }}
-                onClick={() => navigate("/register")}
+                onClick={() => navigate("/termsandconditions")}
+                _hover={{ textDecoration: "none", color: "orange.600" }}
               >
-                Join Now
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                color="orange.500"
+                onClick={() => navigate("/privacypolicy")}
+                _hover={{ textDecoration: "none", color: "orange.600" }}
+              >
+                Privacy Policy
               </Link>
             </Text>
           </VStack>
-
-          {loginError && (
-            <Alert status="error" borderRadius="lg">
-              <AlertIcon />
-              <AlertDescription>{loginError}</AlertDescription>
-            </Alert>
-          )}
-
-          <VStack spacing={3} w="full" mt={2}>
-            <FormControl>
-              <InputGroup size="lg">
-                <InputLeftElement pointerEvents="none">
-                  <Icon as={MdEmail} color="gray.400" />
-                </InputLeftElement>
-                <Input
-                  type="email"
-                  placeholder="Email Address"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  borderColor="gray.200"
-                  _hover={{ borderColor: "orange.400" }}
-                  _focus={{
-                    borderColor: "orange.500",
-                    boxShadow: "0 0 0 1px orange.500",
-                  }}
-                />
-              </InputGroup>
-            </FormControl>
-
-            <FormControl>
-              <InputGroup size="lg">
-                <InputLeftElement pointerEvents="none">
-                  <Icon as={MdLock} color="gray.400" />
-                </InputLeftElement>
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  borderColor="gray.200"
-                  _hover={{ borderColor: "orange.400" }}
-                  _focus={{
-                    borderColor: "orange.500",
-                    boxShadow: "0 0 0 1px orange.500",
-                  }}
-                />
-              </InputGroup>
-            </FormControl>
-
-            <Flex w="full" justify="flex-end">
-              <Link
-                color="orange.500"
-                fontSize="sm"
-                fontWeight="semibold"
-                _hover={{ color: "orange.600" }}
-                onClick={() => navigate("/reset-password")}
-              >
-                Forgot Password?
-              </Link>
-            </Flex>
-
-            <Button
-              w="full"
-              size="lg"
-              colorScheme="orange"
-              isLoading={isLoading}
-              onClick={() => handleSignIn("email")}
-              _hover={{ bg: "orange.600" }}
-            >
-              Continue
-            </Button>
-
-            <HStack w="full" my={1}>
-              <Divider />
-              <Text fontSize="sm" color="gray.500" whiteSpace="nowrap">
-                OR
-              </Text>
-              <Divider />
-            </HStack>
-
-            <Button
-              w="full"
-              size="lg"
-              variant="outline"
-              leftIcon={<Icon as={FcGoogle} boxSize={5} />}
-              borderColor="gray.200"
-              _hover={{ bg: "gray.50" }}
-              onClick={() => handleSignIn("google")}
-              isLoading={isLoading}
-            >
-              Continue with Google
-            </Button>
-          </VStack>
-
-          <Text fontSize="sm" color="gray.500" textAlign="center" mt={2}>
-            By joining, you agree to our{" "}
-            <Link color="orange.500">Terms of Service</Link> and{" "}
-            <Link color="orange.500">Privacy Policy</Link>
-          </Text>
-        </VStack>
+        </Container>
       </Flex>
     </Flex>
   );
